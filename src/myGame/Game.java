1eec8e84be43d8c75410db74c079d9f6fc88e86a
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2023-09-27 15:40:31 +0530

    2.user inputs

--- src/myGame/Game.java
+++ src/myGame/Game.java
@@ -1,5 +1,8 @@
 package myGame;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Game {
     public static void main(String args[]){
         char[][]box={
@@ -8,8 +11,22 @@ public class Game {
                 {' ','|',' ','|',' '},
                 {'-','+','-','+','-'},
                 {' ','|',' ','|',' '}};
+
+//        users turn using scanner
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your location of placement:");
+        int locaton1=input.nextInt();
+        getuserinput();
+
+//        computers turn using random
+        Random rand=new Random();
+        int location2=rand.nextInt(9)+1;
+
+        getuserinput();
         gamelayout(box);
     }
+
+//    methord for printing
     public static  void gamelayout(char[][]box){
         for (int i=0;i<5;i++){
             for (int j=0;j<5;j++){
@@ -18,4 +35,5 @@ public class Game {
             System.out.println();
         }
     }
+    public static void getuserinput(){}
 }
