36f3f12a7558ebea2a15ba56ce9003b671758933
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2023-09-27 16:29:06 +0530

    3.added functionality

--- src/myGame/Game.java
+++ src/myGame/Game.java
@@ -15,25 +15,47 @@ public class Game {
 //        users turn using scanner
         Scanner input = new Scanner(System.in);
         System.out.println("enter your location of placement:");
-        int locaton1=input.nextInt();
-        getuserinput();
+        int location1=input.nextInt();
+        getuserinput(box,location1,"user");
 
 //        computers turn using random
         Random rand=new Random();
         int location2=rand.nextInt(9)+1;
+        getuserinput(box,location2,"computer");
 
-        getuserinput();
         gamelayout(box);
     }
 
 //    methord for printing
     public static  void gamelayout(char[][]box){
-        for (int i=0;i<5;i++){
-            for (int j=0;j<5;j++){
+//        box.length giving num of rows
+//        box[0].length giving num of columns
+        for (int i=0;i<box.length;i++){
+            for (int j=0;j<box.length;j++){
                 System.out.print(box[i][j]);
             }
             System.out.println();
         }
     }
-    public static void getuserinput(){}
+//    added functionality
+    public static void getuserinput(char[][]box,int location,String type){
+        char symbol=' ';
+        if(type.equals("user")){
+            symbol='X';
+        }
+        if(type.equals("computer")){
+            symbol='0';
+        }
+        switch (location){
+            case 1:box[0][0]=symbol;break;
+            case 2:box[0][2]=symbol;break;
+            case 3:box[0][4]=symbol;break;
+            case 4:box[2][0]=symbol;break;
+            case 5:box[2][2]=symbol;break;
+            case 6:box[2][4]=symbol;break;
+            case 7:box[4][0]=symbol;break;
+            case 8:box[4][2]=symbol;break;
+            case 9:box[4][4]=symbol;break;
+        }
+    }
 }
